ad3e6df1a8460382a5eb9a5fc23417745c456185
icjayson <henryhenrry100@gmail.com>
2025-09-10 12:17:37 +0700

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 
 const inter = Inter({ 
@@ -42,6 +43,7 @@ export default function RootLayout({
       </head>
       <body className={`${inter.variable} font-body antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
