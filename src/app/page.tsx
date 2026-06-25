5246f3af4d1c0692d56932c726c291e7ecf470cc
icjayson <henryhenrry100@gmail.com>
2026-06-26 01:41:42 +0700

    Change background video type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Page() {
         x5-playsinline="true"
       >
         <source
-          src="https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260328_083109_283f3553-e28f-428b-a723-d639c617eb2b.mp4"
+          src="/background-video.mp4"
           type="video/mp4"
         />
       </video>
